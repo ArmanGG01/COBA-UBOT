8bcc093ac23f4b8d52ef3ad33cefe967b78c513f
ARMAN0012 <96458836+ARMAN0012@users.noreply.github.com>
2022-03-20 08:45:12 +0700

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -71,7 +71,7 @@ async def quott_(event):
         file = await create_quotly(reply_, bg=match, reply=replied_to, sender=user)
     except Exception as er:
         return await msg.edit(str(er))
-    message = await reply.reply("Quotly by RAM-UBOT", file=file)
+    message = await reply.reply("Quotly by KARMAN-USERBOT", file=file)
     os.remove(file)
     await msg.delete()
     return message
